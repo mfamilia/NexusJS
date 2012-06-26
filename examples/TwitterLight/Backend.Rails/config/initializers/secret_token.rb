e5e3f37e23694fd517cd6ced36f8e3699eaf70c8
Manuel Familia <manuelgfx@gmail.com>
2012-06-25 21:40:02 -0400

    initial commit

---
+++ examples/TwitterLight/Backend.Rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackendRails::Application.config.secret_token = '10afc143d8db46ba3606a512ed42a9257bbc67730e6a4a80b8dc2999fa9ebb914166ac4e459fc6b21e6e1b8ec238f770640e48e484228456563b5018bec8673e'
